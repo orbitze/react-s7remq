46c252f9333e5672abb8e149863f8727a20b1b0b
orbitze <mverma1427@gmail.com>
2022-05-29 16:51:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-s7remq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-s7remq)
---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const jsx = 'JSX is awesome';
+  const [queryArray, set_queryArray] = useState([]);
+  const [dataFields, set_dataFields] = useState([]);
+  const [count, set_count] = useState(0);
+
+  const handleQuery = () => {
+    console.log('handled');
+    set_queryArray(...queryArray, { key: '', value: '' });
+
+    set_count(count + 1);
+    const dataField = (
+      <>
+        Key
+        <input
+          type="text"
+          id={count}
+          onChange={(e) => {
+            const newArr = queryArray;
+            const index = e.target.id;
+            newArr[index].key = e.target.value;
+            set_queryArray(queryArray);
+          }}
+        />
+        Value
+        <input
+          type="text"
+          onChange={() => {
+            const newArr = queryArray;
+            const index = e.target.id;
+            newArr[index].key = e.target.value;
+            set_queryArray(queryArray);
+          }}
+        />
+      </>
+    );
+
+    set_dataFields([...dataFields, dataField]);
+  };
+  return (
+    <div>
+      <span>query-params</span>
+      <br />
+      <button onClick={handleQuery}>Add</button>
+      <br />
+
+      {dataFields}
+      <br />
+      <button>Say</button>
+    </div>
+  );
+}
